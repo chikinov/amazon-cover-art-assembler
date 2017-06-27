108c9ee6ac2e33d9bdc217a7114aefed0f94b7d5
Ken Lau <chikinov@gmail.com>
2017-06-27 18:29:21 +0800

    Use jimp as the image processing library

--- assemble.js
+++ assemble.js
@@ -1,8 +1,7 @@
 const fs = require('fs');
-const gm = require('gm').subClass({ imageMagick: true });
 const http = require('http');
+const Jimp = require('jimp');
 const path = require('path');
-const { Transform } = require('stream');
 
 if (process.argv.length < 3) {
     console.log('Usage: node ' + path.basename(__filename) + ' asin');
@@ -11,38 +10,21 @@ if (process.argv.length < 3) {
     var asin = process.argv[2];
 }
 
-function assembleImages(x = 0, y = 0) {
-    http.get('http://z2-ec2.images-amazon.com/R/1/a=' + asin + '+c=A17SFUTIVB227Z+d=_SCR(3,' + x + ',' + y + ')', function(res) {
-        if (res.statusCode != 200)
-            return;
+function assembleImages(x = 0, y = 0, image = new Jimp(1448, 1448)) {
+    Jimp.read('http://z2-ec2.images-amazon.com/R/1/a=' + asin + '+c=A17SFUTIVB227Z+d=_SCR(3,' + x + ',' + y + ')', function (err, img) {
+        if (err) throw err;
 
-        var data = new Transform();
-        res.on('data', function(chunk) {
-            data.push(chunk);
-        });
-        res.on('end', function() {
-            if (!fs.existsSync('temp')) {
-                fs.mkdirSync('temp');
+        image.composite(img, x * 400, y * 400);
+
+        if (++y > 3) {
+            if (++x > 3) {
+                image.write(asin + '.png');
+                return;
+            } else {
+                y = 0;
             }
-            fs.writeFileSync(path.join('temp', x + '-' + y + '.jpg'), data.read(), 'binary');
-            if (++y > 3) {
-                if (++x > 3) {
-                    var g = gm();
-                    for (i = 0; i < 4; i++) {
-                        for (j = 0; j < 4; j++) {
-                            g = g.in('-page', '+' + (i * 400) + '+' + (j * 400)).in(path.join('temp', i + '-' + j + '.jpg'));
-                        }
-                    }
-                    g.mosaic().write(asin + '.png', function (err) {
-                        if (err) console.log(err);
-                    });
-                    return;
-                } else {
-                    y = 0;
-                }
-            }
-            assembleImages(x, y);
-        });
+        }
+        assembleImages(x, y, image);
     });
 }
 
